9a3f72441de9cac1ab15ee17ec3ad4d24cb947f2
Yuval Avram <yuvalavramG@gmail.com>
2017-01-28 12:20:55 +0200

    Add files via upload

---
+++ mainApp.js
@@ -0,0 +1,23 @@
+var app = angular.module("mainApp", ["ngRoute"]);
+app.config(function($routeProvider) {
+	$routeProvider
+	.when("/", {
+		templateUrl : "general.html",
+		activetab: 'general'
+	})
+	.when("/general", {
+		templateUrl : "general.html",
+		activetab: 'general'
+	})
+	.when("/integrations", {
+		templateUrl : "integrations.html",
+		activetab: 'integrations'
+	})
+});
+
+app.controller('menuCtrl', ['$scope', '$route', '$location',  function($scope, $route, $location){
+	$scope.$route = $route;
+	$scope.changeView = function(view){
+		$location.path(view); 
+	}
+}]);
